db52b4f92e395d0412425209d64c534bfcfa3cca
klequis <klequis@gmail.com>
2018-12-09 11:57:04 -0800

    improved footer layout

--- src/ui/Footer/Footer.jsx
+++ src/ui/Footer/Footer.jsx
@@ -7,35 +7,71 @@ import iFacebook from 'media/logos/facebook.svg'
 import iSlack from 'media/logos/slack.svg'
 import iContact from 'media/contact02.svg'
 
+const meetupURL = 'https://www.meetup.com/trivalleycoders/'
+const gitHubURL = '"https://www.meetup.com/trivalleycoders/"'
+const facebookURL = 'https://www.facebook.com/groups/free.code.camp.sanramon/'
+const slackURL = 'https://join.slack.com/t/trivalleycoders/shared_invite/enQtMjk2NDY3NDAwMjI1LWU0YjFjNjE5MDgwYzYwYmUwMWJlNjk1NDU4YmI5ZmZjZGU0ZDcwY2E2Y2RlNmU0MWFlZTUyODFkYzM1NGVlYTQ'
+
 const Footer = ({ classes }) => {
   return (
     <section className={classes.wrapper}>
-      <a href="https://www.meetup.com/trivalleycoders/"><img src={iMeetup} className={classes.logo} alt='meetup logo' /></a>
-      <a href="https://github.com/klequis"><img src={iGithub} className={classes.logo} alt="klequis' github" /></a>
-      <a href="https://www.facebook.com/groups/free.code.camp.sanramon/"><img src={iFacebook} className={classes.logo} alt='tri valley coders facebook' /></a>
-      <a href="https://join.slack.com/t/trivalleycoders/shared_invite/enQtMjk2NDY3NDAwMjI1LWU0YjFjNjE5MDgwYzYwYmUwMWJlNjk1NDU4YmI5ZmZjZGU0ZDcwY2E2Y2RlNmU0MWFlZTUyODFkYzM1NGVlYTQ"><img src={iSlack} className={classes.logo} alt='tri valley coders slack' /></a>
-      <div className={classes.contact}>
-        <img src={iContact} className={classes.contactIcon} alt='contact carl' />
-        <span className={classes.contactEmail}>carl@trivalleycoders.com</span>
+      <div className={classes.inner}>
+        <div className={classes.links}>
+          <div className={classes.linkRow}>
+            <a href={meetupURL}><img src={iMeetup} className={classes.logo} alt='meetup logo' /></a>
+            <a href={gitHubURL}><img src={iGithub} className={classes.logo} alt="klequis' github" /></a>
+          </div>
+          <div className={classes.linkRow}>
+            <a href={facebookURL}><img src={iFacebook} className={classes.logo} alt='tri valley coders facebook' /></a>
+            <a href={slackURL}><img src={iSlack} className={classes.logo} alt='tri valley coders slack' /></a>
+          </div>
+        </div>
+        <div className={classes.contact}>
+          <img src={iContact} className={classes.contactIcon} alt='contact carl' />
+          <span className={classes.contactEmail}>carl@trivalleycoders.com</span>
+        </div>
       </div>
     </section>
   )
 }
 
-const styles = {
+const styles = theme => ({
   wrapper: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    padding: '3rem 0',
     backgroundColor: '#24292e',
+  },
+  inner: {
+    maxWidth: 751,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: '3rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  links: {
+    display: 'flex',
+    flexDirection: 'column',
+    [theme.breakpoints.up(500)]: {
+      flexDirection: 'row',
+    },
+  },
+  linkRow: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    // eslint-disable-next-line
+    justifyContent: 'space-evenly',
+    width: '100%',
+    marginBottom: '3rem',
+    [theme.breakpoints.up(500)]: {
+      justifyContent: 'space-around',
+    }
   },
   logo: {
     maxHeight: 60,
   },
   contact: {
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   contactIcon: {
     maxHeight: 20,
@@ -45,5 +81,6 @@ const styles = {
     fontWeight: 500,
     marginTop: '1rem',
   },
-}
+})
+
 export default injectSheet(styles)(Footer)
