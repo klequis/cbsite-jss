deb25f2932ad94da14f27b8d2d81037c223d6c54
klequis <klequis@gmail.com>
2018-12-17 12:30:19 -0800

    fix wording & typo

--- src/ui/ThomsonReuters/ThomsonReuters.jsx
+++ src/ui/ThomsonReuters/ThomsonReuters.jsx
@@ -11,7 +11,7 @@ const title = (
 
 const body = (
   <ParagraphWrapper>
-    <Text variant='body1' marginBottom={false}>I was at Thomson Reuters for 3 years as a Product Manager where I worked with clients at Deutsche Bank, Morgan Stanley, Credit Suisse, Nomura, and Rothschild to develop Thomson Reuters MS Excel to MS PowerPoint linking application which enabled users to use Excel data & graphs with linked data in PowerPoint presentations. Although there is a Microsoft Office native equivalent, we were able to develop a product that was more stable and a feature set more tailored to our clients.</Text>
+    <Text variant='body1' marginBottom={false}>I was at Thomson Reuters for 3 years as a Product Manager where I worked with clients at Deutsche Bank, Morgan Stanley, Credit Suisse, Nomura, and Rothschild to create Thomson Reuters' MS Excel to MS PowerPoint linking application which enabled users to use Excel data & graphs with linked data in PowerPoint presentations. Although there is a Microsoft Office native equivalent, we were able to develop a product that was more stable and with a feature set more tailored to our clients needs.</Text>
   </ParagraphWrapper>
 )
 
