55584b97290ae0c482d537fe8293f72da504d5aa
Carl Becker <klequis@gmail.com>
2023-08-12 12:23:21 -0700

    backup

--- TODO.md
+++ TODO.md
@@ -1,5 +1,10 @@
-The "in" logo is too hard. Just put the icons in the footer under "software developer"
+## The "in" logo is too hard. Just put the icons in the footer under "software developer"
 They will be
 - blog
 - github
 - linkedin
+
+## About me
+Write something about myself
+- skills - or do logos
+- experienced software developer with strong / current JavaScript, ecosystem, something
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,45 +1,62 @@
-import React from 'react'
-import injectSheet from 'react-jss'
-import classNames from 'classnames'
+import React from "react";
+import injectSheet from "react-jss";
+import classNames from "classnames";
+import PropTypes from "prop-types";
 
-const Section = (props) => {
 
-  const { children, classes, className, background='light' } = props
+function mkClasses(classes, className, flexDirection) {
+  
+}
+
+const Section = props => {
+  console.log("props", props);
+  const {
+    children,
+    classes,
+    className,
+    background = "light",
+    // isFlexContainer = false,
+    // flexDirection
+  } = props;
   const clsNames = classNames(
     {
-      [classes.dark]: background === 'dark',
+      [classes.dark]: background === "dark",
+      // [classes.inner]: isFlexContainer
+
     },
-    [
-      [classes.wrapper],
-      className,
-    ]
+    [[classes.wrapper], className]
+  );
 
-  )
-
-return (
+  return (
     <section className={clsNames}>
-      <div className={classes.inner}>
-      {children}
-      </div>
+      <div className={classes.inner}>{children}</div>
     </section>
-  )
-
-}
+  );
+};
 
 const styles = theme => ({
   wrapper: theme.section.xs,
   inner: {
-    display: 'flex',
-    alignContent: 'stretch',
+    // display: "flex",
+    // alignContent: "stretch",
     maxWidth: 1180,
-    margin: 'auto',
+    margin: "auto"
   },
   dark: {
     // backgroundColor: '#2b3137',
     backgroundColor: theme.section.colors.background.dark,
-    color: 'white',
+    color: "white",
     boxShadow: theme.section.shadow.dark
-  },
-})
+  }
+});
 
-export default injectSheet(styles)(Section)
+export default injectSheet(styles)(Section);
+
+Section.propTypes = {
+  children: PropTypes.object.isRequired,
+  classes: PropTypes.object,
+  className: PropTypes.object,
+  background: PropTypes.string,
+  // isFlexContainer: PropTypes.bool,
+  // flexDirection: PropTypes.string
+};
